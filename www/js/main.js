285e159683caef27cbda2bd76d19c3192d3510ad
110619045 <0304phoebe@gmail.com>
2023-10-10 22:12:17 +0800

    time test
    test

--- www/js/main.js
+++ www/js/main.js
@@ -4,33 +4,123 @@ webgazer.showVideoPreview(false) /* shows all video previews */
         .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
 
 
+// //一開始眼動的時間
+// var gazeStartTime = null;
+// var elementUnderCursor = null;
+let currentElement = null; // 用于跟踪当前停留的元素
+// console.log('gazeStartTime0：'+gazeStartTime);
 window.onload = async function() {
-
-    // data();
-
     //start the webgazer tracker
     await webgazer.setRegression('ridge') /* currently must set regression and tracker */
         //.setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-            // console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        .setGazeListener(function(data, timestamp) {
+            console.log('timestamp：'+timestamp);
+            /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
             
-            // if (data.x < 5*vh ){
-            //     console.log("right");
+            if(data == null){
+                return;
+            }
+
+            const fontElements = document.getElementsByTagName('font');
+            var x = data.x;
+            var y = data.y;
+            
+            let startTime = null; // 用于跟踪停留的开始时间
+
+            // console.log('x,y：'+ data.x + ',' + data.y);
+            // console.log('fontElements：'+ fontElements);
+            
+            // 迭代 font 元素，檢查獲得哪個元素
+            for (const fontElement of fontElements) {
+                const rect = fontElement.getBoundingClientRect();
+                // console.log('fontElement：'+ fontElement);
+                // console.log('rect：' rect.left);
+                // console.log('currentElement0：', currentElement);
+
+                if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+                    // 找到匹配的 font 元素
+                    
+                    // console.log('找到匹配的 font 元素：', fontElement);
+                    // console.log('currentElement：', currentElement);
+                    
+                    // if (currentElement === fontElement) {
+                    //     // 如果是同一个元素，检查停留时间
+                    //     const currentTime = new Date().getTime();
+                    //     const elapsedTime = currentTime - startTime;
+                    //     console.log('currentTime:'+ currentTime);
+                    //     console.log('startTime:'+ startTime);
+                    //     console.log('elapsedTime：'+ elapsedTime);
+
+                    //     if (elapsedTime >= 3000) { // 3秒（3000毫秒）
+                    //         // console.log('停留超过三秒的 font 元素：', fontElement);
+                    //     }
+                    // } else {
+                    //     // 否则，重置计时器
+                    //     currentElement = fontElement;
+                    //     startTime = new Date().getTime();
+                    // }
+
+                    const currentTime = new Date().getTime();
+                    const elapsedTime = currentTime - startTime;
+
+                    console.log('currentTime：', currentTime);
+                    console.log('startTime：', startTime);
+                    console.log('elapsedTime：', elapsedTime);
+
+                    if (elapsedTime >= 3000) { // 3秒（3000毫秒）
+                        console.log('停留超过三秒的 font 元素：', fontElement);
+                    }
+                    
+                    // 重置计时器
+                    currentElement = fontElement;
+                    startTime = currentTime;
+
+                }
+            }
+
+            // if(data == null){
+            //     elementUnderCursor = null;
+            //     return;
+            // }else {
+            //     // 獲取眼動位置下的元素
+            //     elementUnderCursor = document.elementFromPoint(data.x, data.y);
+            // }
+            // console.log('elementUnderCursor:'+elementUnderCursor);
+
+
+
+            // console.log('gazeStartTime：'+gazeStartTime);
+
+            //檢查眼動為是否停留在元素上
+            // if(isElementUnderGaze(elementUnderCursor, x, y)){
+                
+            //     if (gazeStartTime === null) {
+            //         gazeStartTime = new Date().getTime();
+            //         // console.log(gazeStartTime);
+            //     } else {
+            //         var currentTime = new Date().getTime();
+            //         var gazeDuration = currentTime - gazeStartTime;
+
+            //         // 停留時間觸發事件
+            //         if (gazeDuration >= 3000) {
+            //             // console.log(elementUnderCursor);
+            //             // console.log('元素上停留超過3秒！');
+
+            //             // 加入hovered
+            //             elementUnderCursor.classList.add('hovered');
+            //             gazeDuration = currentTime - gazeStartTime;
+
+            //         }else{
+            //             elementUnderCursor.classList.remove('hovered');
+            //         }
+            //     }
+            // }else {
+            //     //沒有元素
+            //     gazeStartTime = null;
             // }
 
-            // // 获取眼动位置下的元素
-            var elementUnderCursor = document.elementFromPoint(100, 200);
-            // console.log(elementUnderCursor);
-            // // 移除之前添加的效果类
-            // document.querySelectorAll('.hover-effect-element.hovered').forEach(function(element) {
-            // element.classList.remove('hovered');
-            // });
+            // console.log('gazeStartTime2：'+gazeStartTime);
 
-            // if (elementUnderCursor) {
-            // // 当用户的眼动位置位于某个元素上时，添加效果类
-            // elementUnderCursor.classList.add('hovered');
-            // };
         })
         .saveDataAcrossSessions(true)
         .begin();
@@ -40,14 +130,14 @@ window.onload = async function() {
         //     .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
 
     //Set up the webgazer video feedback.
-    var setup = function() {
-        //Set up the main canvas. The main canvas is used to calibrate the webgazer.
-        var canvas = document.getElementById("plotting_canvas");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        canvas.style.position = 'fixed';
-    };
-    setup();
+    // var setup = function() {
+    //     //Set up the main canvas. The main canvas is used to calibrate the webgazer.
+    //     var canvas = document.getElementById("plotting_canvas");
+    //     canvas.width = window.innerWidth;
+    //     canvas.height = window.innerHeight;
+    //     canvas.style.position = 'fixed';
+    // };
+    // setup();
 
 };
 
@@ -58,12 +148,22 @@ window.onbeforeunload = function() {
     webgazer.end();
 }
 
-/**
- * Restart the calibration process by clearing the local storage and reseting the calibration point
- */
+
+//  Restart the calibration process by clearing the local storage and reseting the calibration point
+
 function Restart(){
     document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     webgazer.clearData();
     ClearCalibration();
     PopUpInstruction();
+}
+
+
+function isElementUnderGaze(element, x, y) {
+    if(element!=null){
+        var elementRect = element.getBoundingClientRect();
+        // console.log(element);
+        // console.log(elementRect.left);
+        return x >= elementRect.left && x <= elementRect.right && y >= elementRect.top && y <= elementRect.bottom;    
+    }
 }
